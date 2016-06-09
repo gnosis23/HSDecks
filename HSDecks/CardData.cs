d99fc1d058108f746cf2d97ebe9884cad324cc62
Bohao Wang <bj050323@gmail.com>
2016-06-09 19:57:11 +0800

    HS decks init

---
+++ HSDecks/CardData.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace HSDecks {
+    class CardData {
+        public async static Task GetCards(List<BasicCard> cards, int cost) {
+            var uri = new Uri("ms-appx:///Assets/cards.json");
+            var sampleFile = await StorageFile.GetFileFromApplicationUriAsync(uri);
+
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            string text = await FileIO.ReadTextAsync(sampleFile, Windows.Storage.Streams.UnicodeEncoding.Utf8);
+
+            var readFromFile = text;
+
+            var jsonMessage = readFromFile;
+
+
+            var serializer = new DataContractJsonSerializer(typeof(CardSets));
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+
+            CardSets Fuck = (CardSets) serializer.ReadObject(ms);
+            var selected = from p in Fuck.Basic
+                           where p.cost == cost && p.img != null
+                           orderby p.playerClass
+                           select p;
+
+            cards.Clear();
+            foreach (var item in selected) {
+                cards.Add(item);
+            }
+        }
+    }
+
+    public class Mechanic {
+        public string name { get; set; }
+    }
+
+    public class BasicCard {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public string faction { get; set; }
+        public string rarity { get; set; }
+        public int cost { get; set; }
+        public string text { get; set; }
+        public string flavor { get; set; }
+        public string artist { get; set; }
+        public bool collectible { get; set; }
+        public string playerClass { get; set; }
+        public string howToGet { get; set; }
+        public string howToGetGold { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+        public List<Mechanic> mechanics { get; set; }
+        public int? attack { get; set; }
+        public int? health { get; set; }
+        public string race { get; set; }
+        public string inPlayText { get; set; }
+        public int? durability { get; set; }
+    }
+
+    public class Mechanic2 {
+        public string name { get; set; }
+    }
+
+    public class ClassicCard {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public string faction { get; set; }
+        public string rarity { get; set; }
+        public int cost { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public string text { get; set; }
+        public string flavor { get; set; }
+        public string artist { get; set; }
+        public bool collectible { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+        public List<Mechanic2> mechanics { get; set; }
+        public string race { get; set; }
+        public int? durability { get; set; }
+        public string playerClass { get; set; }
+        public string inPlayText { get; set; }
+    }
+
+    public class Credit {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public string rarity { get; set; }
+        public int cost { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public string text { get; set; }
+        public bool elite { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+    }
+
+    public class Mechanic3 {
+        public string name { get; set; }
+    }
+
+    public class Naxxrama {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public string rarity { get; set; }
+        public int cost { get; set; }
+        public string text { get; set; }
+        public string flavor { get; set; }
+        public string artist { get; set; }
+        public bool collectible { get; set; }
+        public string playerClass { get; set; }
+        public string howToGet { get; set; }
+        public string howToGetGold { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+        public List<Mechanic3> mechanics { get; set; }
+        public int? attack { get; set; }
+        public int? health { get; set; }
+        public int? durability { get; set; }
+        public string race { get; set; }
+    }
+
+    public class Mechanic4 {
+        public string name { get; set; }
+    }
+
+    public class GoblinsVsGnome {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public int cost { get; set; }
+        public string text { get; set; }
+        public string artist { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+        public int? attack { get; set; }
+        public int? health { get; set; }
+        public string race { get; set; }
+        public List<Mechanic4> mechanics { get; set; }
+        public string rarity { get; set; }
+        public string flavor { get; set; }
+        public bool? collectible { get; set; }
+        public string playerClass { get; set; }
+    }
+
+    public class Mechanic5 {
+        public string name { get; set; }
+    }
+
+    public class Mission {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public string faction { get; set; }
+        public string rarity { get; set; }
+        public int cost { get; set; }
+        public string text { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+        public int? attack { get; set; }
+        public int? health { get; set; }
+        public List<Mechanic5> mechanics { get; set; }
+    }
+
+    public class Promotion {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public string faction { get; set; }
+        public string rarity { get; set; }
+        public int cost { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public string text { get; set; }
+        public string inPlayText { get; set; }
+        public string race { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+    }
+
+    public class Mechanic6 {
+        public string name { get; set; }
+    }
+
+    public class BlackrockMountain {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public int cost { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public string text { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+        public List<Mechanic6> mechanics { get; set; }
+        public string rarity { get; set; }
+        public string artist { get; set; }
+        public string race { get; set; }
+        public string flavor { get; set; }
+        public bool? collectible { get; set; }
+        public string howToGet { get; set; }
+        public string howToGetGold { get; set; }
+        public int? durability { get; set; }
+        public string playerClass { get; set; }
+        public bool? elite { get; set; }
+    }
+
+    public class Mechanic7 {
+        public string name { get; set; }
+    }
+
+    public class TavernBrawl {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public int cost { get; set; }
+        public int attack { get; set; }
+        public int durability { get; set; }
+        public string text { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+        public int? health { get; set; }
+        public List<Mechanic7> mechanics { get; set; }
+        public string race { get; set; }
+    }
+
+    public class Mechanic8 {
+        public string name { get; set; }
+    }
+
+    public class TheGrandTournament {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public string rarity { get; set; }
+        public int cost { get; set; }
+        public string text { get; set; }
+        public string flavor { get; set; }
+        public string artist { get; set; }
+        public bool collectible { get; set; }
+        public string playerClass { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+        public int? attack { get; set; }
+        public int? health { get; set; }
+        public List<Mechanic8> mechanics { get; set; }
+        public string race { get; set; }
+        public int? durability { get; set; }
+    }
+
+    public class Mechanic9 {
+        public string name { get; set; }
+    }
+
+    public class TheLeagueOfExplorer {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public int cost { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public string text { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+        public string rarity { get; set; }
+        public int? durability { get; set; }
+        public string flavor { get; set; }
+        public string artist { get; set; }
+        public bool? collectible { get; set; }
+        public string playerClass { get; set; }
+        public string howToGet { get; set; }
+        public string howToGetGold { get; set; }
+        public List<Mechanic9> mechanics { get; set; }
+        public string race { get; set; }
+        public bool? elite { get; set; }
+    }
+
+    public class Mechanic10 {
+        public string name { get; set; }
+    }
+
+    public class WhispersOfTheOldGod {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public string rarity { get; set; }
+        public int cost { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public string text { get; set; }
+        public string flavor { get; set; }
+        public string artist { get; set; }
+        public bool collectible { get; set; }
+        public string playerClass { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+        public List<Mechanic10> mechanics { get; set; }
+        public string race { get; set; }
+        public int? durability { get; set; }
+        public bool? elite { get; set; }
+    }
+
+    [DataContract]
+    public class CardSets {
+        [DataMember]
+        public List<BasicCard> Basic { get; set; }
+        [DataMember]
+        public List<ClassicCard> Classic { get; set; }
+        [DataMember]
+        public List<Credit> Credits { get; set; }
+        [DataMember]
+        public List<Naxxrama> Naxxramas { get; set; }
+        [DataMember]
+        public List<object> Debug { get; set; }
+        [DataMember(Name = "Goblins vs Gnomes")]
+        public List<GoblinsVsGnome> GoblinsvsGnomes { get; set; }
+        [DataMember]
+        public List<Mission> Missions { get; set; }
+        [DataMember]
+        public List<Promotion> Promotion { get; set; }
+        [DataMember]
+        public List<object> Reward { get; set; }
+        [DataMember]
+        public List<object> System { get; set; }
+        [DataMember(Name = "Blackrock Mountain")]
+        public List<BlackrockMountain> BlackrockMountain { get; set; }
+        [DataMember(Name = "Hero Skins")]
+        public List<object> HeroSkins { get; set; }
+        [DataMember(Name = "Tavern Brawl")]
+        public List<TavernBrawl> TavernBrawl { get; set; }
+        [DataMember(Name = "The Grand Tournament")]
+        public List<TheGrandTournament> TheGrandTournament { get; set; }
+        [DataMember(Name = "The League of Explorers")]
+        public List<TheLeagueOfExplorer> TheLeagueofExplorers { get; set; }
+        [DataMember(Name = "Whispers of the Old Gods")]
+        public List<WhispersOfTheOldGod> WhispersoftheOldGods { get; set; }
+    }
+
+}
---
+++ HSDecks/MainPage.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HSDecks
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public List<BasicCard> Cards;
+        public ObservableCollection<BasicCard> Board;
+
+        int _page = 0;
+        int _cost = 0;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Cards = new List<BasicCard>();
+            Board = new ObservableCollection<BasicCard>();
+        }
+
+        private void IconTextBlock_Click(object sender, RoutedEventArgs e) {
+            MenuView.IsPaneOpen = !MenuView.IsPaneOpen;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e) {
+            refreshPageAsync();
+        }
+
+        private async void refreshPageAsync() {
+            Progress.IsActive = true;
+            Progress.Visibility = Visibility.Visible;
+
+            if ((Cards.Count == 0) 
+                || (Cards[0].cost != _cost)) {
+                Task t = CardData.GetCards(Cards, _cost);
+                await t;
+            }
+
+            List<Image> currentPage = new List<Image>() {
+                    Image0, Image1, Image2, Image3,
+                    Image4, Image5, Image6, Image7
+            };
+
+            if (Cards.Count >= 8) {
+                BasicCard empty = new BasicCard();
+                Board.Clear();
+
+                for (int i = 0; i < 8; i++) {
+                    if (_page * 8 + i < Cards.Count) {
+                        Board.Add(Cards.ElementAt(_page * 8 + i));
+                    } 
+                    else {
+                        empty.img = "ms-appx:///Assets/yaoming.jpg";
+                        Board.Add(empty);
+                    }
+                }
+
+                int count = 0;
+                foreach (Image item in currentPage) {
+                    Uri uri = new Uri(Board[count].img);
+                    BitmapImage image = new BitmapImage(uri);
+
+                    item.Source = image;
+
+                    count++;
+                }
+            } 
+            else {
+                int count = 0;
+                foreach (Image item in currentPage) {
+                    Uri uri = new Uri("ms-appx:///Assets/yaoming.jpg");
+                    BitmapImage image = new BitmapImage(uri);
+
+                    item.Source = image;
+                    count++;
+                }
+            }
+
+            Progress.Visibility = Visibility.Collapsed;
+            Progress.IsActive = false;
+        }
+
+        private void NextPageButton_Click(object sender, RoutedEventArgs e) {
+            if (Cards.Count > 0 && (_page + 1) * 8 < Cards.Count) {
+                _page++;
+            }
+
+            refreshPageAsync();
+        }
+
+        private void PrevPageButton_Click(object sender, RoutedEventArgs e) {
+            if (_page > 0) {
+                _page--;
+            }
+
+            refreshPageAsync();
+        }
+
+        private void Btn0_Click(object sender, RoutedEventArgs e) {
+            var item = (Button)sender;
+            _cost = Int32.Parse(item.Content.ToString());
+            _page = 0;
+
+            refreshPageAsync();
+        }
+
+    }
+}
