2776151e4fcb088bb58dc66ecba7f7b3f0b84d39
Bohao Wang <bj050323@gmail.com>
2016-06-17 13:34:21 +0800

    add deck head control

---
+++ HSDecks/Controls/DeckHeadBlock.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace HSDecks.Controls {
+    public sealed partial class DeckHeadBlock : UserControl {
+        public DeckHeadBlock() {
+            this.InitializeComponent();
+
+            this.DataContextChanged += (s, e) => Bindings.Update();
+        }
+
+        public Models.Deck Deck { get { return this.DataContext as Models.Deck; } }
+    }
+}
--- HSDecks/Models/Deck.cs
+++ HSDecks/Models/Deck.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace HSDecks.Models {
     public class Deck {
@@ -12,10 +13,14 @@ namespace HSDecks.Models {
         public string name { get; set; }
         public PlayerClass playerClass { get; set; }
 
+        public BitmapImage hImage { get; set; }
+
         public Deck(int id, string name, PlayerClass playerClass) {
             this.Id = id;
             this.name = name;
             this.playerClass = playerClass;
+            hImage = new BitmapImage(new Uri(String.Format("ms-appx:///Assets/control/{0}.jpg", 
+                playerClass.ToString())));
         }
     }
 }
